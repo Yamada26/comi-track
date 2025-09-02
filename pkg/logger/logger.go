154b0f16b37a55022f9ca73b342531375d6b0711
akhr-ymd <akhr-ymd@outlook.com>
2025-09-03 00:20:37 +0900

    ログ出力先修正

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,16 +3,27 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"path/filepath"
 )
 
 var Logger *slog.Logger
 
-// Init initializes the global logger
 func init() {
-	logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	// カレントディレクトリの1階層上をプロジェクトルートと仮定する場合
+	// （実行ファイルが cmd/ 下にある場合など）
+	cwd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
+
+	// プロジェクト直下に app.log
+	logPath := filepath.Join(cwd, "app.log")
+
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+
 	Logger = slog.New(slog.NewJSONHandler(logFile, &slog.HandlerOptions{
 		AddSource: true,
 	}))
